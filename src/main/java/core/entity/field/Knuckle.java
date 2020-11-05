6f1ee228f60dc5b5b012bcc8b4bd8f41b7e39a70
Данила Кальнов <danyakalnov@gmail.com>
2020-11-05 18:19:38 +0400

    Implement KnuckleObservable and notify listeners when moved

--- src/main/java/core/entity/field/Knuckle.java
+++ src/main/java/core/entity/field/Knuckle.java
@@ -1,10 +1,15 @@
 package core.entity.field;
 
+import core.event.KnuckleListener;
+import core.event.KnuckleObservable;
 import core.exceptions.CellAlreadyHasKnuckleException;
+import java.util.ArrayList;
+import java.util.List;
 
-public class Knuckle {
+public class Knuckle implements KnuckleObservable {
     private int _number;
     private Cell _position;
+    private List<KnuckleListener> _observers = new ArrayList<>();
 
     public Knuckle(int number) {
         this._number = number;
@@ -44,12 +49,15 @@ public class Knuckle {
         return this._position.getEmptyNeighborCell();
     }
 
-    public Cell move() {
+    protected Cell move() {
         Cell emptyCell = canMove();
         if (emptyCell != null) {
             this._position.removeKnuckle(this);
             try {
-                if (emptyCell.setKnuckle(this)) return emptyCell;
+                if (emptyCell.setKnuckle(this)) {
+                    notifyListeners();
+                    return emptyCell;
+                }
             } catch (CellAlreadyHasKnuckleException exception) {
                 System.out.println(exception.getMessage());
             }
@@ -71,5 +79,22 @@ public class Knuckle {
     @Override
     public String toString() {
         return "Костяшка №" + Integer.toString(this._number);
+    }
+
+    private void notifyListeners() {
+        for (KnuckleListener observer : _observers) {
+            observer.knuckleMoved();
+        }
+    }
+
+    @Override
+    public void registerObserver(KnuckleListener newListener) {
+        if (!_observers.contains(newListener) && newListener != null)
+            _observers.add(newListener);
+    }
+
+    @Override
+    public void removeObserver(KnuckleListener listener) {
+        _observers.remove(listener);
     }
 }
