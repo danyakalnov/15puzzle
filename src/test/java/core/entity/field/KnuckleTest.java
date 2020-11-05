7b5daf0e019fda1f61a8672555e9b931c6e76f1b
Данила Кальнов <danyakalnov@gmail.com>
2020-11-05 22:34:06 +0400

    Positive and negative tests for setCell method

--- src/test/java/core/entity/field/KnuckleTest.java
+++ src/test/java/core/entity/field/KnuckleTest.java
@@ -1,5 +1,7 @@
 package core.entity.field;
 
+import core.util.Point;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,7 +9,37 @@ import static org.junit.jupiter.api.Assertions.*;
 class KnuckleTest {
 
     @Test
-    void setCell() {
+    void setCellPositive() {
+        Point point = new Point(0, 0);
+        Cell cell = new Cell(point);
+
+        final int knuckleNumber = 1;
+        Knuckle knuckle = new Knuckle(knuckleNumber);
+
+        boolean result;
+        result = knuckle.setCell(cell);
+        Cell actualCell = knuckle.getCell();
+
+        Assertions.assertTrue(result);
+        Assertions.assertEquals(cell, actualCell);
+    }
+
+    @Test
+    void setCellNegative() {
+        Point firstPoint = new Point(0, 0);
+        Cell firstCell = new Cell(firstPoint);
+        Point secondPoint = new Point(1, 1);
+        Cell secondCell = new Cell(secondPoint);
+
+        final int knuckleNumber = 1;
+        Knuckle knuckle = new Knuckle(knuckleNumber);
+
+        knuckle.setCell(firstCell);
+        Cell actualCell = knuckle.getCell();
+        boolean result = knuckle.setCell(secondCell);
+
+        Assertions.assertFalse(result);
+        Assertions.assertEquals(firstCell, actualCell);
     }
 
     @Test
