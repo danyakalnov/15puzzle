afc1169ab593323c3f2fb95047d42eb90c362891
Данила Кальнов <danyakalnov@gmail.com>
2020-11-05 22:28:05 +0400

    Create test class for Knuckle

---
+++ src/test/java/core/entity/field/KnuckleTest.java
@@ -0,0 +1,24 @@
+package core.entity.field;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KnuckleTest {
+
+    @Test
+    void setCell() {
+    }
+
+    @Test
+    void removeCell() {
+    }
+
+    @Test
+    void canMove() {
+    }
+
+    @Test
+    void move() {
+    }
+}
