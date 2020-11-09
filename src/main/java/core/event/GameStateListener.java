6b7c559327029648af5ef4f60b57b83e71572b9e
Данила Кальнов <danyakalnov@gmail.com>
2020-11-09 16:19:12 +0400

    Pass moved knuckle instance and new knuckle position for knuckle and game state listeners

--- src/main/java/core/event/GameStateListener.java
+++ src/main/java/core/event/GameStateListener.java
@@ -1,5 +1,8 @@
 package core.event;
 
+import core.entity.field.Cell;
+import core.entity.field.Knuckle;
+
 public interface GameStateListener {
-    void knuckleMoved(boolean isGameOver, int movesCount);
+    void knuckleMoved(boolean isGameOver, int movesCount, Knuckle movedKnuckle, Cell newKnucklePosition);
 }
--- src/main/java/core/event/KnuckleListener.java
+++ src/main/java/core/event/KnuckleListener.java
@@ -1,5 +1,12 @@
 package core.event;
 
+import core.entity.field.Cell;
+import core.entity.field.Knuckle;
+
 public interface KnuckleListener {
-    void knuckleMoved();
+    /**
+     * Оповещение слушателей о перемещении костяшки
+     * @param knuckle костяшка, изменившая позицию на поле
+     */
+    void knuckleMoved(Knuckle knuckle, Cell newKnucklePosition);
 }
