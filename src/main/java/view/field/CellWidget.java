be51fb78ba96d312f74725560413e47c132d4a27
Данила Кальнов <danyakalnov@gmail.com>
2020-11-09 16:19:46 +0400

    Widget for Cell

--- src/main/java/view/field/CellWidget.java
+++ src/main/java/view/field/CellWidget.java
@@ -3,15 +3,47 @@ package view.field;
 import core.entity.field.Cell;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class CellWidget extends JPanel {
     private Cell cell;
     private KnuckleWidget knuckle;
+    private Dimension dimension;
 
-    CellWidget(Cell cell) {
+    CellWidget(Cell cell, Dimension dim, Color color) {
+        super();
         this.cell = cell;
-
+        this.dimension = dim;
+        setPreferredSize(dim);
+        setLayout(null);
+        setBackground(color);
         this.knuckle = new KnuckleWidget(cell.getKnuckle());
+        knuckle.setBounds(0, 0, dim.width, dim.height);
         add(knuckle);
+    }
+
+    public int getKnuckleNumber() {
+        return knuckle.getKnuckleNumber();
+    }
+
+    public KnuckleWidget removeKnuckle() {
+        remove(knuckle);
+        repaint();
+        return knuckle;
+    }
+
+    public Cell getCell() {
+        return this.cell;
+    }
+
+    public void setKnuckle(KnuckleWidget knuckle) {
+        this.knuckle = knuckle;
+        add(knuckle);
+        repaint();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
     }
 }
