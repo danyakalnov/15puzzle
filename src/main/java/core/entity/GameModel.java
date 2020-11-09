e59c5de01d5a80c73440d63539dc40f14a8b25fe
Данила Кальнов <danyakalnov@gmail.com>
2020-11-09 16:16:11 +0400

    Pass moved knuckle and new knuckle position in knuckleMoved method

--- src/main/java/core/entity/GameModel.java
+++ src/main/java/core/entity/GameModel.java
@@ -2,6 +2,7 @@ package core.entity;
 
 import core.entity.field.Cell;
 import core.entity.field.GameField;
+import core.entity.field.Knuckle;
 import core.event.GameStateListener;
 import core.event.KnuckleListener;
 import core.event.KnuckleObservable;
@@ -35,7 +36,8 @@ public class GameModel implements KnuckleListener, ModelObservable {
         this._field = new GameField(_fieldSize);
         List<KnuckleObservable> knucklesList = this._field.getCells().stream().map(Cell::getKnuckle).collect(Collectors.toList());
         for (KnuckleObservable knuckle : knucklesList) {
-            knuckle.registerObserver(this);
+            if(knuckle != null)
+                knuckle.registerObserver(this);
         }
     }
 
@@ -52,7 +54,7 @@ public class GameModel implements KnuckleListener, ModelObservable {
 
         for (int i = 0; i < _fieldSize * _fieldSize; ++i) {
             int currentKnuckleNumber = knucklesArrangement.get(i);
-            if (currentKnuckleNumber == 0) inversionsCount += 1 + i / _fieldSize; // TODO: возможно, прибавление 1 влечёт за собой ошибку! Обратить на это внимание!!!
+            if (currentKnuckleNumber == 0) inversionsCount += 1 + i / _fieldSize;
         }
 
         return inversionsCount % 2 == 0;
@@ -65,29 +67,32 @@ public class GameModel implements KnuckleListener, ModelObservable {
             correctKnucklesOrder.add(i);
         }
 
+
+
         List<Cell> cellList = this._field.getCells();
         List<Integer> knucklesNumbers = new ArrayList<Integer>();
         cellList.forEach(cell -> {
             knucklesNumbers.add(cell.getKnuckleNumber());
         });
 
-        return correctKnucklesOrder.equals(knucklesNumbers);
+        return correctKnucklesOrder.equals(knucklesNumbers.subList(0, knucklesNumbers.size() - 1));
     }
 
     public GameField getGameField() {
         return this._field;
     }
 
-    private void notifyObservers(boolean isGameOver, int movesCount) {
+    private void notifyObservers(boolean isGameOver, int movesCount, Knuckle movedKnuckle, Cell newKnucklePosition) {
         for (GameStateListener listener : this._listeners)
-            listener.knuckleMoved(isGameOver, movesCount);
+            listener.knuckleMoved(isGameOver, movesCount, movedKnuckle, newKnucklePosition);
     }
 
     @Override
-    public void knuckleMoved() {
+    public void knuckleMoved(Knuckle movedKnuckle, Cell newKnucklePosition) {
         this._movesCount += 1;
         this._gameOver = areKnucklesInOrder();
-        notifyObservers(this._gameOver, this._movesCount);
+        System.out.println(movedKnuckle.toString() + " переместилась в " + newKnucklePosition.toString());
+        notifyObservers(this._gameOver, this._movesCount, movedKnuckle, newKnucklePosition);
     }
 
     @Override
